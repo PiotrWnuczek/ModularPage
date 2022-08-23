dd4081f52c3579d516cb9013f1af7e6b3db8a98f
Piotr Wnuczek <piotrwnuczekpl@gmail.com>
2022-08-23 19:08:10 +0200

    options

--- src/atoms/WebsiteOptions.js
+++ src/atoms/WebsiteOptions.js
@@ -1,9 +1,29 @@
 import React, { useState } from 'react';
-import { Box, Dialog, Button, Divider } from '@mui/material';
-import { Typography, Avatar } from '@mui/material';
-import { Settings } from '@mui/icons-material';
+import { Box, Grid, Typography } from '@mui/material';
+import { Dialog, Button, Avatar } from '@mui/material';
+import { ToggleButtonGroup, ToggleButton } from '@mui/material';
+import { FormatSize, Settings } from '@mui/icons-material';
+import { Colorize, ColorLens } from '@mui/icons-material';
+import { HexColorPicker } from 'react-colorful';
 
 const WebsiteOptions = ({ website }) => {
+  const [titleSize, setTitleSize] = useState('large');
+  const [textSize, setTextSize] = useState('large');
+  const size = {
+    title: [titleSize, setTitleSize],
+    text: [textSize, setTextSize],
+  };
+  const [titleColor, setTitleColor] = useState('#444444');
+  const [textColor, setTextColor] = useState('#555555');
+  const [buttonColor, setButtonColor] = useState('#1565c0');
+  const [backgroundColor, setBackgroundColor] = useState('#f5f5f5');
+  const color = {
+    title: ['title', titleColor, setTitleColor],
+    text: ['text', textColor, setTextColor],
+    button: ['button', buttonColor, setButtonColor],
+    background: ['background', backgroundColor, setBackgroundColor],
+  };
+  const [picker, setPicker] = useState(color.title);
   const [open, setOpen] = useState(false);
 
   return (
@@ -32,24 +52,54 @@ const WebsiteOptions = ({ website }) => {
           >
             {website && website.name} Website Settings
           </Typography>
-          <Box>
-            <Typography>
-              Text <br />
-              Color <br />
-              Background <br />
-              Components
-            </Typography>
-            <Divider />
-            <Typography>
-              Standard <br />
-              Wide <br />
-              Narrow
-            </Typography>
+          <Box sx={{ py: 2 }}>
+            <Grid container>
+              <Grid item xs={12} md={7}>
+                {['title', 'text', 'button', 'background'].map(item =>
+                  <Box
+                    sx={{ my: 1, display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+                    onClick={() => setPicker(color[item])}
+                    key={item}
+                  >
+                    <Avatar sx={{ bgcolor: color[item][1] }}>
+                      {item === picker[0] ? <Colorize /> : <ColorLens />}
+                    </Avatar>
+                    <Typography sx={{ ml: 1, textTransform: 'capitalize' }}>
+                      {item} color picker
+                    </Typography>
+                  </Box>
+                )}
+              </Grid>
+              <Grid item xs={12} md={5}>
+                <HexColorPicker color={picker[1]} onChange={picker[2]} />
+              </Grid>
+            </Grid>
+            {['title', 'text'].map(item =>
+              <Box sx={{ my: 1 }} key={item}>
+                <ToggleButtonGroup
+                  value={size[item][0]}
+                  onChange={(e) => size[item][1](e.target.value)}
+                  color='primary'
+                  size='small'
+                >
+                  <ToggleButton value='large'>
+                    <FormatSize /> Large {item}
+                  </ToggleButton>
+                  <ToggleButton value='small'>
+                    <FormatSize /> Small {item}
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </Box>
+            )}
           </Box>
           <Button
             sx={{ mt: 1 }}
             onClick={() => {
-              console.log('set');
+              console.log({
+                titleSize, textSize,
+                titleColor, textColor,
+                buttonColor, backgroundColor,
+              });
               setOpen(false);
             }}
             variant='contained'
@@ -58,8 +108,8 @@ const WebsiteOptions = ({ website }) => {
             Set
           </Button>
         </Box>
-      </Dialog>
-    </Box>
+      </Dialog >
+    </Box >
   )
 };
 
